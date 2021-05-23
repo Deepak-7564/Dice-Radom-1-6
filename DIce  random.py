d75f9d113f6a779534530eb557bef8dca4a8ecb7
Deepak <ydeepak7564@gmail.com>
2021-05-23 13:14:22 +0530

    adding program files

---
+++ DIce  random.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  8 21:45:38 2020
+
+@author: Asus
+"""
+import random
+
+x = "y"
+while x == "y":
+
+    no = random.randint(1,6)
+    if no == 1:
+        print("[-----]") 
+        print("[     ]") 
+        print("[  0  ]") 
+        print("[     ]") 
+        print("[-----]") 
+    if no == 2: 
+        print("[-----]") 
+        print("[ 0   ]") 
+        print("[     ]") 
+        print("[   0 ]") 
+        print("[-----]") 
+    if no == 3: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[     ]") 
+        print("[-----]") 
+    if no == 4: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[     ]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 5: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[  0  ]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 6: 
+        print("[-----]") 
+        print("[0 0 0]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[-----]") 
+        
+    x = input("press y to roll again and n to exit: ")
+    print("\n")
